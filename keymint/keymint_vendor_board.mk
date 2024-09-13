c89e37cf869083ae77f524f67c70ee9a5b7e8997
Kishor Chappa <quic_kchappa@quicinc.com>
2024-09-12 22:58:40 -0700

    Revert "Change to disable ese."
    This reverts commit c773e3228c05927aff9c29ecf3bd624227a92747.
    
    Reason for revert: Changes are part of latest mainline branch
    
    Change-Id: I62ba8845c9f599e8e4dde81181ce8c78b9e55ca6

--- keymint/keymint_vendor_board.mk
+++ keymint/keymint_vendor_board.mk
@@ -7,9 +7,9 @@ endif
 
 #monaco specific build rules
 ifeq ($(TARGET_BOARD_PLATFORM),monaco)
-TARGET_USES_ESE_KEYMINT := false
-ENABLE_ESE_KEYMINT_SPLIT_MANIFEST := false
-ENABLE_ESE_KEYMINT_FEATURE_XML := false
+TARGET_USES_ESE_KEYMINT := true
+ENABLE_ESE_KEYMINT_SPLIT_MANIFEST := true
+ENABLE_ESE_KEYMINT_FEATURE_XML := true
 endif
 
 #Camano specific build rules
