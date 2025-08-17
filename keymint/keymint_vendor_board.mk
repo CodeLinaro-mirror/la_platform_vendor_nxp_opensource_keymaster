aa16ceece79df6005dbd6565bb23e7361a7d23bd
Om Prakash Singh <quic_omprsing@quicinc.com>
2025-08-17 12:28:30 +0530

    keymint: Enable dynamic activation via init script
    1) Keep keymint and weaver services disabled by default; enable them at
       runtime using init.qti.ese.strongbox.sh.
    
    2) For platforms without the script (e.g., Canoe), OEMs should enable services by default.
    
    3) Comment out VINTF entry as it's already enabled via common VINTF.
    
    4) Disable authsecret service since it's optional.
    
    Change-Id: I6e1669981f7819c86d6500758a70477307ffadd7

--- keymint/keymint_vendor_board.mk
+++ keymint/keymint_vendor_board.mk
@@ -1,6 +1,7 @@
 #pineapple specific build rules
 ifeq ($(TARGET_BOARD_PLATFORM),pineapple)
 TARGET_USES_ESE_KEYMINT := false
+TARGET_USES_ESE_AUTHSECRET := false
 ENABLE_ESE_KEYMINT_SPLIT_MANIFEST := false
 ENABLE_ESE_KEYMINT_FEATURE_XML := false
 endif
@@ -8,13 +9,16 @@ endif
 #sun specific build rules
 ifeq ($(TARGET_BOARD_PLATFORM),sun)
 TARGET_USES_ESE_KEYMINT := false
+TARGET_USES_ESE_AUTHSECRET := false
 ENABLE_ESE_KEYMINT_SPLIT_MANIFEST := false
 ENABLE_ESE_KEYMINT_FEATURE_XML := false
 endif
 
-#canoe specific build rules
+#canoe and alor specific build rules
+#enable for alor by default. alor uses same lunch command
 ifeq ($(TARGET_BOARD_PLATFORM),canoe)
-TARGET_USES_ESE_KEYMINT := false
+TARGET_USES_ESE_KEYMINT := true
+TARGET_USES_ESE_AUTHSECRET := false
 ENABLE_ESE_KEYMINT_SPLIT_MANIFEST := false
 ENABLE_ESE_KEYMINT_FEATURE_XML := false
 endif
--- weaver/weaver_vendor_board.mk
+++ weaver/weaver_vendor_board.mk
@@ -10,8 +10,9 @@ TARGET_USES_ESE_WEAVER := false
 ENABLE_ESE_WEAVER_SPLIT_MANIFEST := false
 endif
 
-#canoe specific build rules
+#canoe and alor specific build rules
+#enable for alor by default. alor uses same lunch command
 ifeq ($(TARGET_BOARD_PLATFORM),canoe)
-TARGET_USES_ESE_WEAVER := false
+TARGET_USES_ESE_WEAVER := true
 ENABLE_ESE_WEAVER_SPLIT_MANIFEST := false
 endif
